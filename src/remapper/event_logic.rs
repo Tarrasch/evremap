2b76c7ba54b170d73aeadf1258b5e99220f55c60
Arash Rouhani <arash.rouhani@gmail.com>
2025-02-27 15:48:45 +0100

    Re-commit changes I reverted by mistake

--- src/remapper/event_logic.rs
+++ src/remapper/event_logic.rs
@@ -31,10 +31,21 @@ pub fn is_modifier(key: &KeyCode) -> bool {
     }
 }
 
-pub fn get_key_using_mapping(mappings: &Vec<Mapping>, code: KeyCode) -> KeyCode {
+pub fn get_key_using_mapping<'a>(
+    mappings: &'a Vec<Mapping>,
+    currently_pressed_modifiers: &HashSet<Modifier>,
+    code: KeyCode,
+) -> KeyCode {
+    // Arash note: I changed the original logic to a simple linear search. We prioritize the first match rather than the one with the most matching "input".
     mappings
         .iter()
-        .find(|Mapping::Remap { input, .. }| input == &code)
+        .find(
+            |Mapping::Remap {
+                 input, modifiers, ..
+             }| {
+                input == &code && currently_pressed_modifiers.is_superset(modifiers)
+            },
+        )
         .map(|Mapping::Remap { output, .. }| output)
         .unwrap_or(&code)
         .clone()
@@ -43,25 +54,54 @@ pub fn get_key_using_mapping(mappings: &Vec<Mapping>, code: KeyCode) -> KeyCode 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashSet;
 
     mod get_key_using_mapping {
         use super::*;
-        
         #[test]
         fn should_passthrough_when_mappings_list_is_empty() {
             let mappings = vec![];
-            let result = get_key_using_mapping(&mappings, KeyCode::KEY_A);
-            assert_eq!(result, KeyCode::KEY_A);
+            let currently_pressed_modifiers = HashSet::from([Modifier::LeftAlt]);
+            let result =
+                get_key_using_mapping(&mappings, &currently_pressed_modifiers, KeyCode::KEY_A);
+            assert!(result == KeyCode::KEY_A);
         }
 
         #[test]
-        fn should_convert_simple_mapping() {
+        fn should_convert_when_no_modifier_needed() {
             let mappings = vec![Mapping::Remap {
                 input: KeyCode::KEY_A,
+                modifiers: HashSet::from([]),
                 output: KeyCode::KEY_B,
             }];
-            let result = get_key_using_mapping(&mappings, KeyCode::KEY_A);
-            assert_eq!(result, KeyCode::KEY_B);
+            let result = get_key_using_mapping(&mappings, &HashSet::from([]), KeyCode::KEY_A);
+            assert!(result == KeyCode::KEY_B);
+        }
+
+        #[test]
+        fn should_not_convert_when_missing_modifier_needed() {
+            let mappings = vec![Mapping::Remap {
+                input: KeyCode::KEY_A,
+                modifiers: HashSet::from([Modifier::LeftAlt]),
+                output: KeyCode::KEY_B,
+            }];
+            let result = get_key_using_mapping(&mappings, &HashSet::from([]), KeyCode::KEY_A);
+            assert!(result == KeyCode::KEY_A);
+        }
+
+        #[test]
+        fn should_convert_when_enough_modifiers_needed() {
+            let mappings = vec![Mapping::Remap {
+                input: KeyCode::KEY_A,
+                modifiers: HashSet::from([Modifier::LeftAlt]),
+                output: KeyCode::KEY_B,
+            }];
+            let result = get_key_using_mapping(
+                &mappings,
+                &HashSet::from([Modifier::LeftAlt]),
+                KeyCode::KEY_A,
+            );
+            assert!(result == KeyCode::KEY_B);
         }
     }
 }
